6025f30a0a88facb96e5e5bfab1d8ac913c0c603
Peter Husman <petermhusman@gmail.com>
2023-08-08 15:15:35 -0700

    Created new project script

--- app.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "ev3api.h"
-
-extern void main_task(intptr_t ptr);
-
---
+++ ev3rt-new.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+mkdir "$1"
+cd "$1"
+wget https://github.com/PeterHusman/EV3RTProjectTemplate/releases/latest/download/project.zip
+unzip project.zip
+rm project.zip
